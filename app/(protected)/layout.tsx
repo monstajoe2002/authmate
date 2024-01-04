d1e18f3298c031c5ba8fba7f3fc253de58fbd118
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-01-04 19:23:47 +0200

    design settings page, basic for now

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import { Navbar } from "./_components/navbar";
+
+interface ProtectedLayoutProps {
+  children: ReactNode;
+}
+
+export default function ProtectedLayout({ children }: ProtectedLayoutProps) {
+  return (
+    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-blue-800">
+      <Navbar />
+      {children}
+    </div>
+  );
+}
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,19 @@
-import { auth, signOut } from "@/auth";
-export default async function SettingsPage() {
-  const session = await auth();
+"use client";
+
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+export default function SettingsPage() {
+  const user = useCurrentUser();
+  const onClick = () => {
+    logout();
+  };
   return (
-    <div>
-      <pre>
-        <code>{JSON.stringify(session, null, 2)}</code>
-      </pre>
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign Out</button>
+    <div className="bg-white p-10 rounded-xl">
+      <form>
+        <button onClick={onClick} type="submit">
+          Sign Out
+        </button>
       </form>
     </div>
   );
