a6ca377efb576d5ed7c37d209442fa7e0b1983fd
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-01-05 12:45:02 +0200

    add examples using client and server components

--- auth.ts
+++ auth.ts
@@ -50,6 +50,9 @@ export const {
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
       return session;
     },
     async jwt({ token }) {
@@ -60,6 +63,7 @@ export const {
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     },
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,8 +1,9 @@
 import { UserRole } from "@prisma/client";
 import NextAuth, { type DefaultSession } from "next-auth";
 
-export type ExtendedUser = DefaultSession["user"] & {
+type ExtendedUser = DefaultSession["user"] & {
   role: UserRole;
+  isTwoFactorEnabled: boolean;
 };
 
 declare module "next-auth" {
