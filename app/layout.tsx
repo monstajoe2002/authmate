13d250a5d027754bd2cce1349a149e98cb0bdd3f
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-01-05 13:39:08 +0200

    add permissions for admins

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Toaster />
         <SessionProvider session={session}>{children}</SessionProvider>
       </body>
     </html>
